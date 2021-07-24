ccd3a69a49caa5e14590241b921eec53ea829e76
Alexey <al77ex1@gmail.com>
2021-07-24 21:06:57 +0300

    modified validation

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -1,5 +1,5 @@
 const objectId = (value, helpers) => {
-  if (!value.match(/^[0-9a-fA-F]{24}$/)) {
+  if (!value.match(/^[0-9a-fA-F-]{36}$/)) {
     return helpers.message('"{{#label}}" must be a valid id');
   }
   return value;
